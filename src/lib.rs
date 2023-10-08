99e094830fd56ec4732a20a616819836d9fa052d
Core <daverencordero@gmail.com>
2023-10-08 23:50:23 +0800

    feat: added lib mounting functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    println!("Hello, {}!", name);
+pub fn greet(name: &str) -> String {
+    let formatter_str = format!("Hello, {}!", name);
+    formatter_str
+}
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, {useEffect, useState} from 'react';
+import './app.scss';
+import init, {greet} from "./snake-lib"
 
 function App() {
+    const [libMounted, setLibMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        init().then(() => setLibMounted(true));
+    }, []);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            {libMounted && greet("World")}
         </div>
     );
 }
